f0f247a3965d0b4a06549bd8221b8973f8758cd3
sunil singh <chaudharysunil62@gmail.com>
2022-12-15 17:57:52 +0530

    commit refresh code

--- lib/compute.dart
+++ lib/compute.dart
@@ -8,18 +8,12 @@ class ComputeService {
   Future<List<Model>> fetchUser() async {
     String response =
         await Api.getUser("https://jsonplaceholder.typicode.com/comments");
-
     return await compute(deserializeJsons, response);
   }
-
   List<Model> deserializeJsons(String response) {
     // return List<Model>.fromJson(jsonDecode(response));
 
     return List<Model>.from(
         json.decode(response).map((x) => Model.fromJson(x)));
   }
-
-
-
-  
 }
